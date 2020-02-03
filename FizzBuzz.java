d3e060736f9bb3379511f356e2dc1f24c8a96ab3
Spooky-png <MikeHinds4@gmail.com>
2020-02-03 13:10:58 -0800

    FizzBuzz practice

---
+++ FizzBuzz.java
@@ -0,0 +1,16 @@
+public class FizzBuzz {
+    public String fizzBuzzer(int number) {
+        if(number%15 == 0){
+            return "FizzBuzz";
+        }
+        if(number%5 == 0){
+            return "Buzz";
+        }
+        if(number%3 == 0){
+            return "Fizz";
+        }
+        else{
+            return ""+number;
+        }
+    }
+}
---
+++ FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz fb = new FizzBuzz();
+        int number = 3;
+        String answer = fb.fizzBuzzer(number);
+        System.out.println(answer);
+    }
+}
